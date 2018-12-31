5d374a31e0a7eaddfe96ae1268b38a43dd99e5a0
Avik <aviknandi@yahoo.com>
2018-12-31 18:11:59 +0530

    targeted each destination and send the the target to show up how many location belong to that destination

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -28,11 +28,14 @@ class Main extends CI_Controller {
 
 	// all location for single destination
 	public function allLocation(){
+		$something = $this->input->get('id');
+		$a["destination_id"] = $something;
 
 		$data['active']='allLocation';  //header data(we are not using it but it is need to avoid error)
 		$data['siteData'] = $this->Model->getOneRow('sitedata'); //site global details
-
-		$data['allLocationDetails'] = $this->Model->joinLocationDestination();
+		$data["match"] = $this->db->get_where("locations",$a)->result();
+		echo "<pre>";
+		print_r($data);
 
 		$this->load->template('common/header','destination/all-location','common/footer', $data);
 	}
